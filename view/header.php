7033779d7e00797ef59f074886b8f2cc9b683028
Tomy-my <azerouto@gmail.com>
2021-09-11 13:51:20 +0200

    First commit

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Lebazar est un site d'agance de voyage'">
+    <meta name="author" content="Tomy SOTTY" />
+    <link rel="stylesheet" href="./public/css/style.css" />
+    <link rel="stylesheet" href="./public/css/fonts.css" />
+    <link rel="stylesheet" href="./public/css/@query.css" />
+    <link rel="icon" href="./public/images/mainLogo.svg" />
+    <script src="https://kit.fontawesome.com/99371baa88.js" crossorigin="anonymous"></script>
+    <title>Evatrip : <?php echo $titre; ?></title>
+</head>
+
+<body>
---
+++ view/navigation.php
@@ -0,0 +1,47 @@
+<nav>
+    <div class="ct_left_nav">
+        <div class="container_logo">
+            <a href="./index.php">
+                <img id="_logo" src="./public/images/mainLogo2.svg" alt="Logo représentant Evatrip">
+            </a>
+        </div>
+    </div>
+    <div class="ct_mid_nav">
+        <div class="container_nav _ct_nav_d">
+            <a href="./index.php">
+                <div class="logo_cat">
+                    <i class="fas fa-map-marked-alt"></i>
+                </div>
+                Destinations <i class="fas fa-chevron-down"></i>
+            </a>
+            <div class="underline _ud"></div>
+        </div>
+        <div class="container_nav _ct_nav_c">
+            <a href="./index.php">
+                <div class="logo_cat">
+                    <i class="fas fa-anchor"></i>
+                </div>
+                Croisières
+            </a>
+            <div class="underline _uc"></div>
+        </div>
+        <div class="container_nav _ct_nav_h">
+            <a href="./index.php">
+                <div class="logo_cat">
+                    <i class="fas fa-hotel"></i>
+                </div>
+                Hôtels
+            </a>
+            <div class="underline _uh"></div>
+        </div>
+        <div class="container_nav _ct_nav_g">
+            <a href="./index.php">
+                Galerie
+            </a>
+            <div class="underline _ug"></div>
+        </div>
+    </div>
+    <div class="ct_bot_nav">
+
+    </div>
+</nav>
