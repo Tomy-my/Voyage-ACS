82aa33187eaaf478c8804180c4cee5c6903fac59
Tomy-my <azerouto@gmail.com>
2021-09-15 15:42:47 +0200

    Footer V1

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,38 @@
+<footer>
+    <div class="footer_left">
+        <div class="copyright">
+            © 2021 Evatrip, Inc.
+        </div>
+        <div class="style_corner sc_right">
+            ·
+        </div>
+        <div class="conf">
+            <a href="#">Confidentialité </a>
+        </div>
+        <div class="style_corner sc_left">
+            ·
+        </div>
+        <div class="cg">
+            <a href="#">Conditions générales</a>
+        </div>
+    </div>
+    <div class="footer_mid">
+
+    </div>
+    <div class="footer_right">
+        <div class="footer_logo">
+            <img src="./public/images/mainLogo2.svg" alt="Logo du site">
+        </div>
+        <div class="footer_social">
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            <a href="#"><i class="fab fa-github"></i></a>
+        </div>
+    </div>
+</footer>
+
 <script type="text/javascript" src="./public/javascript/button_bird.js"></script>
-    </body>
+</body>
+
 </html>
